cb8050570cfbff2f5e42245dd2780a23c3644543
Naeem <naeemark@gmail.com>
2020-08-20 18:41:43 +0800

    Adds swagger #6, #13

--- src/main/java/com/naeemark/jas/task/controller/HealthController.java
+++ src/main/java/com/naeemark/jas/task/controller/HealthController.java
@@ -2,6 +2,8 @@ package com.naeemark.jas.task.controller;
 
 import com.naeemark.jas.task.JwtAuthServiceApplication;
 import com.naeemark.jas.task.model.Health;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +18,14 @@ import static com.naeemark.jas.task.utils.Constants.SERVICE_NAME;
  * <p>
  * Created on: 2020-08-20
  */
+@Api(tags = "Auth Service Health")
 @RestController
 @RequestMapping("/api")
 public class HealthController {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthServiceApplication.class);
 
+    @ApiOperation(value = "Health Check", notes = "Gets health status of the service", response = Health.class)
     @GetMapping(value = "/health")
     public Health checkHealth() {
 
